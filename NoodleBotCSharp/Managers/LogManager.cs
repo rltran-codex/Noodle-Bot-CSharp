438bab4c61ec8aabad58b9d40b8d40af2b02dfbf
rltran-codex <rtran9@montgomerycollege.edu>
2024-05-08 09:01:08 -0400

    logging implemented, bot connects and starts

---
+++ NoodleBotCSharp/Managers/LogManager.cs
@@ -0,0 +1,66 @@
+﻿using Discord;
+
+using Serilog;
+using Serilog.Events;
+
+using System;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace NoodleBotCSharp.Managers
+{
+  public class LogManager
+  {
+    // Private static instance variable
+    private static LogManager _instance;
+
+    private LogManager()
+    {
+      var logfn = $"noodlebot_{DateTime.Now.ToString("yyyyMMdd")}.txt";
+      var logFile = Path.Combine(
+        Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData),
+        "NoodleBot",
+        "Logs",
+        logfn
+        );
+      Log.Logger = new LoggerConfiguration()
+        .WriteTo.Console()
+        .WriteTo.File(logFile)
+        .CreateLogger();
+
+      Log.Write(LogEventLevel.Information, "Logger successfully initialized");
+      Log.Write(LogEventLevel.Information, $"Log file can be found at: {logFile}");
+    }
+
+    public static LogManager Instance
+    {
+      get
+      {
+        if (_instance == null)
+        {
+          _instance = new LogManager();
+        }
+        return _instance;
+      }
+    }
+
+    public static Task LogAsync(LogMessage msg)
+    {
+      return Task.Run(() =>
+      {
+        var severity = msg.Severity switch
+        {
+          LogSeverity.Critical => LogEventLevel.Fatal,
+          LogSeverity.Error => LogEventLevel.Error,
+          LogSeverity.Warning => LogEventLevel.Warning,
+          LogSeverity.Info => LogEventLevel.Information,
+          LogSeverity.Verbose => LogEventLevel.Verbose,
+          LogSeverity.Debug => LogEventLevel.Debug,
+          _ => LogEventLevel.Information
+        };
+
+        Log.Write(severity, msg.Exception, "[{Source}] {Message}", msg.Source, msg.Message);
+      });
+    }
+  }
+}
--- NoodleBotCSharp/Program.cs
+++ NoodleBotCSharp/Program.cs
@@ -1,1 +1,44 @@
 ﻿
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NoodleBotCSharp.Managers;
+using Serilog;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NoodleBotCSharp
+{
+  public class Program
+  {
+    // Main entry point for starting the Discord Bot "Noodle Bot"
+    public static async Task Main(string[] args)
+    {
+      var log = LogManager.Instance;
+
+      // build host service
+      var builder = new HostBuilder()
+        .UseSerilog() // attaching Serilog
+        .ConfigureAppConfiguration((hostingContext, config) =>
+        {
+          // configurations for app
+          config.AddEnvironmentVariables();
+        })
+        .ConfigureServices((hostContext, services) =>
+        {
+          // services for app
+          services.AddSingleton<IHostedService, NoodleBotService>();
+        });
+
+      try
+      {
+        await builder.RunConsoleAsync();
+      }
+      catch (OperationCanceledException ocex)
+      {
+        Console.WriteLine(ocex.Message);
+      }
+    }
+  }
+}
